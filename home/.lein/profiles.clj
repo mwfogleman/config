02277f8563ef87c90fd03ba3e43272e964066799
Michael Fogleman <michaelwfogleman@gmail.com>
2014-09-02 08:47:47 -0400

    Clojure: Add refresh-namespace functionality.
    Necessary changes in lein, added function, and key binding.

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-try "0.4.3"]]}}
+                  [lein-try "0.4.3"]]
+        :dependencies [[org.clojure/tools.namespace "0.2.5"]]}}}
