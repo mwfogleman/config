2ef431f2e4bb64a2d731f648dccb3bf312a9b42a
Michael Fogleman <michaelwfogleman@gmail.com>
2014-09-12 13:04:46 -0400

    Clojure: Add lein midje.

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-try "0.4.3"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.5"]]}}}
+                  [lein-try "0.4.3"]
+                  [lein-midje "3.1.3"]]
+        :dependencies [[org.clojure/tools.namespace "0.2.5"]]}}
