20235c986781fb0f6e5198341a3c6e5c3d868205
Michael Fogleman <michaelwfogleman@gmail.com>
2014-10-05 09:21:08 -0400

    Rocinante: Edit backup script.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo 'Mail'
-sudo rsync --progress --delete -avz /home/michael/Maildir/ /mnt/Maildir/;
+# To add a new file:
+# sudo rsync --progress --delete -avz /home/michael/Downloads/file.pdf /mnt/
 
-echo 'Photos'
-sudo rsync --progress --delete -avz /home/michael/photos/ /mnt/photos/;
+# To add a new folder:
+# sudo rsync --progress --delete -avz /home/michael/music/ /mnt/music/
 
-echo 'Music'
-sudo rsync --progress --delete -avz /home/michael/music/ /mnt/music/;
+# Then add two lines in this script, mimicing the entries here:
 
-echo 'Movies'
-sudo rsync --progress --delete -avz /home/michael/movies/ /mnt/movies/;
+echo 'Anki'
+sudo rsync --progress --delete -avz /home/michael/Documents/Anki/ /mnt/Anki/;
 
 echo 'Ebooks'
 sudo rsync --progress --delete -avz /home/michael/ebooks/ /mnt/ebooks/;
@@ -18,5 +17,17 @@ sudo rsync --progress --delete -avz /home/michael/ebooks/ /mnt/ebooks/;
 echo 'Keep In Touch'
 sudo rsync --progress --delete -avz /home/michael/Dropbox/keepintouch.data /mnt/;
 
+echo 'Mail'
+sudo rsync --progress --delete -avz /home/michael/Maildir/ /mnt/Maildir/;
+
+echo 'Movies'
+sudo rsync --progress --delete -avz /home/michael/movies/ /mnt/movies/;
+
+echo 'Music'
+sudo rsync --progress --delete -avz /home/michael/music/ /mnt/music/;
+
 echo 'Passwords'
 sudo rsync --progress --delete -avz /home/michael/passwords.org.gpg /mnt/;
+
+echo 'Photos'
+sudo rsync --progress --delete -avz /home/michael/photos/ /mnt/photos/;
